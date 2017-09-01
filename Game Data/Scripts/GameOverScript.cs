4321bd6e0c0488ab15e8a8bec95c94d153626f3e
vintisachdeva <vintisachdeva17@gmail.com>
2017-09-01 12:45:44 +0530

    Add files via upload

---
+++ Game Data/Scripts/GameOverScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+	public PlayerHealth playerHealth;      
+	public float restartDelay = 5f;        
+
+	Animator anim;                         
+	float restartTimer; 
+
+	void Awake ()
+	{
+		anim = GetComponent <Animator> ();
+	}
+
+
+	void Update ()
+	{
+		if(playerHealth.currentHealth <= 0)
+		{
+			anim.SetTrigger ("GameOver");
+
+			restartTimer += Time.deltaTime;
+
+			if(restartTimer >= restartDelay)
+			{
+				SceneManager.LoadScene ("Game");
+			}
+		}
+	}
+
+}
